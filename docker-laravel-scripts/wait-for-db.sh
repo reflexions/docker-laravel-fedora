50926ebab6f54327354d58c925f38faa8a17a121
Greg Martyn <greg.martyn@gmail.com>
2020-12-11 12:50:51 -0500

    default to postgres like we were before

--- docker-laravel-scripts/wait-for-db.sh
+++ docker-laravel-scripts/wait-for-db.sh
@@ -5,11 +5,10 @@ set -x #echo on
 # http://unix.stackexchange.com/a/149053/47781
 # this weird syntax is just bash being bash. It uses the DB_HOST value if set, localhost otherwise
 # http://www.tldp.org/LDP/abs/html/parameter-substitution.html
-db_driver=${DB_DRIVER-mysql}
+db_connection=${DB_CONNECTION-pgsql}
 
-[[ $db_driver = 'mysql' ]] && default_db_host="mysql" || default_db_host="postgres"
-[[ $db_driver = 'mysql' ]] && default_db_port="3306" || default_db_port="5432"
-db_host=${DB_HOST-$default_db_host}
+[[ $db_connection = 'pgsql' ]] && default_db_port="5432" || default_db_port="3306"
+db_host=${DB_HOST-db}
 db_port=${DB_PORT-$default_db_port}
 
 echo "Waiting for DB to accept connections on $db_host/$db_port"
