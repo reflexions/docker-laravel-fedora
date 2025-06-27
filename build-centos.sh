f48c5c2c66d4180562f74462e24df2004bb01f72
Greg Martyn <greg.martyn@gmail.com>
2025-06-27 16:28:34 -0400

    add more cache env vars, have centos-10 specify subarch /v3

--- build-centos.sh
+++ build-centos.sh
@@ -2,7 +2,23 @@
 # OS=centos-9 PHP=${PHP_VERSION-8.3} NODE=${NODE_MAJOR_VERSION-20} ./cloud-build-local.sh
 
 build_arg="--build-arg PHP_VERSION=${PHP_VERSION-8.3} --build-arg NODE_MAJOR_VERSION=${NODE_MAJOR_VERSION-22}"
-docker build --progress plain . -f Dockerfile-centos-9 --target php-base $build_arg || exit 1
-docker build --progress plain . -f Dockerfile-centos-9 --target with-node $build_arg || exit 1
-docker build --progress plain . -f Dockerfile-centos-9 --target with-gcloud $build_arg || exit 1
-echo "Success"
+
+pids=()
+centos_versions=( 8 9 10 )
+for version in "${centos_versions[@]}"; do
+	docker build --progress plain . -f Dockerfile-centos-${version} --target with-gcloud $build_arg &
+	pids[${version}]=$!
+done
+
+# wait for all pids
+failed=false
+for pid in "${pids[@]}"; do
+	wait $pid || { echo "$pid failed"; failed=true; }
+done
+
+if [ $failed ]; then
+	echo "Failed"
+	exit 1
+else
+	echo "Success"
+fi
