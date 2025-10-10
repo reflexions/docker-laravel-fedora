08b4537146e695931572b51244c917eb621e20fd
Greg Martyn <greg.martyn@gmail.com>
2025-10-10 11:56:47 -0400

    stop using an env var for dnf bin

--- docker-laravel-scripts/xdebug-install.sh
+++ docker-laravel-scripts/xdebug-install.sh
@@ -6,10 +6,10 @@
 # the touch is per https://bugzilla.redhat.com/show_bug.cgi?id=1213602
 # it's needed for every yum operation when the host is using overlayfs (like macs and GCR)
 touch /var/lib/rpm/*
-${DNF} -y install \
+dnf -y install \
     iproute \
     php-pecl-xdebug3
-${DNF} clean all
+dnf clean all
 
 echo "xdebug.mode=debug" >> /etc/php.d/15-xdebug.ini
 echo "xdebug.start_with_request=yes" >> /etc/php.d/15-xdebug.ini
